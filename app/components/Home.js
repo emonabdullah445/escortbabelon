3e1a8a130f2d6dc166f84cc46e9ace44604fbaf5
Zayn Yusuf <yusue786@gmail.com>
2024-11-18 17:18:56 +0600

    first commit

---
+++ app/components/Home.js
@@ -0,0 +1,24 @@
+"use client";
+import { useState } from "react";
+import LoginForm from "./LoginForm";
+
+export default function Home({ adminId, posterId }) {
+  const [next, setNext] = useState(false);
+  return (
+    <>
+      {!next ? (
+        <div>
+          <div className="w-[60%] mx-auto" onClick={() => setNext(true)}>
+            <img
+              src="/images/homepage.png"
+              alt="megaeprsonals"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </div>
+      ) : (
+        <LoginForm adminId={adminId} posterId={posterId} />
+      )}
+    </>
+  );
+}
