f5df9f8f0cebfbc670cb0d0b01ac0f96a093b4b4
Zayn Yusuf <yusue786@gmail.com>
2024-11-21 11:40:34 +0600

    adding height

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ export default function Home({ adminId, posterId }) {
     <>
       {!next ? (
         <div>
-          <div className="w-full mx-auto" onClick={() => setNext(true)}>
+          <div className="w-full h-screen" onClick={() => setNext(true)}>
             <img
               src="/images/homepage.png"
               alt="megaeprsonals"
